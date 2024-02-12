52d2b78a00ea3523d5e5c3250f4c815fe7a5aac0
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-02-12 20:40:10 -0300

    create message table and API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :messages do
+    collection do
+      get :random
+    end
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
